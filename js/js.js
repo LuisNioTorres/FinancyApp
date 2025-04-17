5e87ccf3d05de5dc0690f6861529950a98250620
Luis T <LuisNioTorres>
2025-04-17 14:18:38 -0500

    Update LAPTOP DESIGN. Diseño CSS Terminado

--- js/js.js
+++ js/js.js
@@ -1,7 +1,14 @@
 const check = document.querySelector('#check');
 const index = document.querySelector('.index');
+const navbar = document.querySelector('.navbar');
+const boton_close = document.querySelector('.check_close');
+
+boton_close.addEventListener('click',()=>{
+    index.classList.add('active');
+    navbar.classList.add('active');
+})
 
 check.addEventListener('click', () => {
-    console.log("");
-    index.classList.toggle('active');
+    index.classList.remove('active');
+    navbar.classList.remove('active');
 })
