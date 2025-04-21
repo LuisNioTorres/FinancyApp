71a7b996590e234068cc1cd9dabfa0df2171f02c
Luis T <LuisNioTorres>
2025-04-20 20:44:11 -0500

    Validaciones de Formulario: KeyDown, Inputs, EnviarFormulario

--- js/js.js
+++ js/js.js
@@ -1,3 +1,5 @@
+import { App } from './app.js'; 
+
 const boton_open = document.querySelector('#check');
 const index = document.querySelector('.index');
 const navbar = document.querySelector('.navbar');
@@ -5,6 +7,24 @@ const boton_close = document.querySelector('.check_close');
 const home = document.querySelector('.home');
 const options = document.querySelectorAll('.option');
 const options_transacciones = document.querySelectorAll('.atajos__div');
+
+let expresiones_tecla = {
+    nombre_transaccion: /^[a-zA-Z0-9_ ]+$/,
+    monto_transaccion : /^[0-9.]$/
+}
+
+let expresiones_input = {
+    nombre_transaccion: /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\s.,\-()]+$/,
+    monto_transaccion: /^[0-9,\.]+$/,
+    fecha_transaccion: /^\d{4}-\d{2}-\d{2}$/
+};
+
+const validaciones = {
+    nombre_transaccion : false,
+    monto_transaccion : false,
+    fecha_transaccion : false
+}
+
 
 
 class UI {
@@ -73,33 +93,118 @@ class Navbar {
     }
 }
 
+
 class Transacciones {
     static mostrarFormulario (options_transacciones) {
         options_transacciones.forEach((transaccion) => {
-            transaccion.addEventListener('click', () => {
-                UI.cargarHtml(home,`formulario`);
+            transaccion.addEventListener('click', async () => {
+                await UI.cargarHtml(home,`formulario`);
+                const inputs = document.querySelectorAll('.input');
+                const fecha = document.querySelector('#fecha_transaccion');
+                const boton_enviar = document.querySelector('.boton_enviar');
+                Formulario.aplicarValidacion(inputs);
+                Formulario.enviarFormulario(boton_enviar,inputs);
+
             })
         })
     }
+
 }
+
+class Formulario {
+    static aplicarValidacion (inputs) {
+        inputs.forEach(input => {
+            if(input.name=="monto_transaccion"){
+                input.addEventListener('keyup',()=>Formulario.mostrarMoneda(input));
+            }
+
+            input.addEventListener('keydown', (e) =>  {
+            Formulario.validarTecla(e,expresiones_tecla[input.name])
+            })
+        });
+    }
+
+    static mostrarMoneda(input) {
+        //Permitir solo Numeros o punto.
+        let value = input.value.replace(/[^0-9.]/g,'');
+
+        //Separar el value donde haya un punto.
+
+        let partes = value.split('.');
+
+        //Permitir un solo punto .
+        if(partes.length>2){
+            value = partes[0] + '.' + partes[1];
+        }
+
+        //Separar el value en parte ENTERA y DECIMAL
+        let[entera,decimal] = value.split('.');
+
+        //Convertir la parte entera en MONEDA US
+        entera = Number(entera).toLocaleString('en-US');
+
+
+        //Si decimal es indefinico, simplemente queda la parte entera,
+        //Si hemos colocado decimal, puesto mostramos ENTERA.DECIMAL
+        input.value = decimal==undefined? entera : entera+'.'+decimal;
+    }
+
+    static validarTecla (event,expresion) {
+        const valueKey = event.key;
+
+        if(valueKey=='Backspace' || valueKey.length>1) return;
+
+        if(!expresion.test(valueKey)) event.preventDefault();
+
+    }
+
+    static enviarFormulario (boton_enviar,inputs) {
+        boton_enviar.addEventListener('click', (e) => {
+            e.preventDefault();
+            inputs.forEach(input=> Formulario.validarInputs(input,expresiones_input[input.name]));
+            if(validaciones.fecha_transaccion == true &&
+               validaciones.monto_transaccion == true &&
+               validaciones.fecha_transaccion == true
+            ) {
+                Formulario.mostrarMensaje('.formulario__mensaje_exito','active',600);
+                inputs.forEach((input)=>{
+                    input.classList.add('exito')
+                    setTimeout(()=>{
+                        input.classList.remove('exito')
+                    },600)
+                })
+
+            } else {
+                Formulario.mostrarMensaje('.formulario__mensaje_error','active',900);
+                
+            }
+        })
+    }
+
+    static mostrarMensaje (clase,estado,tiempo) {
+        const mensaje = document.querySelector(clase);
+        mensaje.classList.add(estado);
+        setTimeout(()=>{
+            mensaje.classList.remove(estado)
+        },tiempo)
+    }
+   
+    static validarInputs(input,expresion){
+        validaciones[input.name] = expresion.test(input.value) ? true : false;
+        //Modificar los Colores de los inputs correctos o incorrectos
+        if(!expresion.test(input.value)){
+            input.classList.add('active')
+            setTimeout(()=>{
+                input.classList.remove('active');
+            },600)
+        } else {
+            input.classList.remove('active');
+        }
+    }
+
+}
+
 
 UI.mostrarNavbar([boton_open,boton_close],[index,navbar]);
 UI.mostrarIndex(options);
 Transacciones.mostrarFormulario(options_transacciones);
-/*/STYLES DE CADA BOTON Y MODIFICAR EL INDEX
-options.forEach((option)=>{
-    option.addEventListener('click', ()=>{
-        options.forEach(o => o.classList.remove('active'));
-        option.classList.add('active');
-
-        const enlace = option.querySelector('p').textContent;
-
-        fetch(`${enlace}.html`)
-        .then(res => res.text()) // OJO: no es res.json(), porque es HTML plano
-        .then(html => {
-          home.innerHTML = html;
-        });
-
-    })
-})
-*/
